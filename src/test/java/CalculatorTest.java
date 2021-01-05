771ce619d3417e3b8919a1939be5cbd229abaa44
lushpurr <lushpurr@gmail.com>
2021-01-05 16:18:37 +0000

    initial test setup

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator1;
+
+    @Before
+    public void setUp(){ calculator1= new Calculator(4, 6); }
+
+    @Test
+    public void canAddTwoNumbers(){
+        assertEquals(10, calculator1.addTwoNumbers());
+    }
 }
